836568e548b3ac5259bc0578f7c34d3f1b47bca0
abrar510 <mdabrarkhondokar@gmail.com>
2026-06-13 21:39:20 +0600

    database.py

--- database.py
+++ database.py
@@ -1,51 +1,7 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
+from pymongo import MongoClient
+from config import MONGO_URI
 
-client = AsyncIOMotorClient(
-    os.getenv("MONGO_URI")
-)
+client = MongoClient(MONGO_URI)
 
-db = client["file_store_bot"]
-
-files_col = db["files"]
-batches_col = db["batches"]
-
-
-async def save_file(file_id, message_id):
-
-    await files_col.update_one(
-        {"file_id": file_id},
-        {
-            "$set": {
-                "message_id": message_id
-            }
-        },
-        upsert=True
-    )
-
-
-async def get_file(file_id):
-
-    return await files_col.find_one(
-        {"file_id": file_id}
-    )
-
-
-async def save_batch(batch_id, message_ids):
-
-    await batches_col.update_one(
-        {"batch_id": batch_id},
-        {
-            "$set": {
-                "message_ids": message_ids
-            }
-        },
-        upsert=True
-    )
-
-
-async def get_batch(batch_id):
-
-    return await batches_col.find_one(
-        {"batch_id": batch_id}
-    )
+db = client["FileStoreBot"]
+files = db["files"]
